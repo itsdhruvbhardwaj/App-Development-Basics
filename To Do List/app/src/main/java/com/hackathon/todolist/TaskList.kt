2a8175cb7557addc89f1d6116809955a9761bb77
realdhruvsharmaa <itsdhruvbhardwaj@gmail.com>
2025-11-23 19:20:21 +0530

    Task Dilog is added

--- To Do List/app/src/main/java/com/hackathon/todolist/TaskList.kt
+++ To Do List/app/src/main/java/com/hackathon/todolist/TaskList.kt
@@ -2,6 +2,7 @@ package com.hackathon.todolist
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import java.nio.file.WatchEvent
 
 data class Tasks(
     val id:Int,
@@ -34,7 +37,7 @@ fun ToDoListApp()
     var tasks by remember { mutableStateOf(listOf<Tasks>()) }
     var showDilog by remember { mutableStateOf(false) }
     var taskName by remember { mutableStateOf("") }
-    var taskDate by remember { mutableStateOf("") }
+    var taskDiscription by remember { mutableStateOf("") }
 
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center)
     {
@@ -58,7 +61,33 @@ fun ToDoListApp()
     if(showDilog)
     {
         AlertDialog(onDismissRequest = { showDilog=false },
-            confirmButton = {},
+            confirmButton = {
+                Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                    Button(onClick = {
+                        if(taskName.isNotBlank())
+                        {
+                            val newTask = Tasks(
+                                id = tasks.size + 1,
+                                name = taskName,
+                                description = taskDiscription,
+                                date = "12/12/2025"
+                            )
+                            tasks = tasks + newTask
+                            showDilog=false
+                            taskName=""
+                            taskDiscription=""
+                        }
+                    }) {
+                        Text("Add")
+                    }
+                    Button(onClick = {showDilog=false}) {
+                        Text("Cancel")
+                    }
+                }
+
+            },
             title = {Text("Add Your Task Here")},
             text = {
                 Column {
@@ -70,8 +99,8 @@ fun ToDoListApp()
                         )
 
                     OutlinedTextField(
-                        value = taskDate,
-                        onValueChange = { taskDate = it },
+                        value = taskDiscription,
+                        onValueChange = { taskDiscription = it },
                         singleLine = true,
                         modifier = Modifier.fillMaxWidth().padding(8.dp)
                     )
