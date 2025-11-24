9cc40ac5c9641e491f7321b6b559f6e7aaf330c3
realdhruvsharmaa <itsdhruvbhardwaj@gmail.com>
2025-11-24 09:36:19 +0530

    Added Lazy Columns for Task list

--- To Do List/app/src/main/java/com/hackathon/todolist/TaskList.kt
+++ To Do List/app/src/main/java/com/hackathon/todolist/TaskList.kt
@@ -1,5 +1,9 @@
 package com.hackathon.todolist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,10 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,8 +24,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import java.nio.file.WatchEvent
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import kotlin.collections.plus
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+
 
 data class Tasks(
     val id:Int,
@@ -35,14 +54,17 @@ data class Tasks(
 fun ToDoListApp()
 {
     var tasks by remember { mutableStateOf(listOf<Tasks>()) }
-    var showDilog by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
+    var editTask by remember { mutableStateOf<Tasks?>(null) }
+
+    var showEditDialog by remember { mutableStateOf(false) }
     var taskName by remember { mutableStateOf("") }
-    var taskDiscription by remember { mutableStateOf("") }
+    var taskDescription by remember { mutableStateOf("") }
 
     Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center)
     {
         Button(
-            onClick = {showDilog=true},
+            onClick = {showDialog=true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text("Add a task");
@@ -54,39 +76,58 @@ fun ToDoListApp()
                 .padding(16.dp)
         )
         {
-
+            items(tasks) {
+                TaskList(it,
+                    onRowClick = {
+                        // Open edit dialog
+                        showEditDialog = true
+                        editTask = it
+                        // Load selected task into fields
+                        taskName = it.name
+                        taskDescription = it.description
+                    },
+                    onDeleteClick = { },
+                    )
+            }
         }
     }
 
-    if(showDilog)
+    if(showDialog)
     {
-        AlertDialog(onDismissRequest = { showDilog=false },
-            confirmButton = {
-                Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+        AlertDialog(onDismissRequest = { showDialog=false }, confirmButton =
+            {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                     horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
+                    )
+                {
+                    Button(onClick = {showDialog=false})
+                    {
+                        Text("Cancel")
+                    }
                     Button(onClick = {
                         if(taskName.isNotBlank())
                         {
+                            val currentDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                                .format(Date())
+
                             val newTask = Tasks(
                                 id = tasks.size + 1,
                                 name = taskName,
-                                description = taskDiscription,
-                                date = "12/12/2025"
+                                description = taskDescription,
+                                date = currentDate
                             )
                             tasks = tasks + newTask
-                            showDilog=false
+                            showDialog=false
                             taskName=""
-                            taskDiscription=""
+                            taskDescription=""
                         }
                     }) {
                         Text("Add")
                     }
-                    Button(onClick = {showDilog=false}) {
-                        Text("Cancel")
-                    }
-                }
 
+                }
             },
             title = {Text("Add Your Task Here")},
             text = {
@@ -94,18 +135,153 @@ fun ToDoListApp()
                     OutlinedTextField(
                         value = taskName,
                         onValueChange = { taskName = it },
+                        label = { Text("Name") },
+                        keyboardOptions = KeyboardOptions(
+                            autoCorrect = true,
+                            capitalization = KeyboardCapitalization.Sentences,
+                            keyboardType = KeyboardType.Text
+                        ),
                         singleLine = true,
-                        modifier = Modifier.fillMaxWidth().padding(8.dp)
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
                         )
 
                     OutlinedTextField(
-                        value = taskDiscription,
-                        onValueChange = { taskDiscription = it },
+                        value = taskDescription,
+                        onValueChange = { taskDescription = it },
+                        label = { Text("Description") },
+                        keyboardOptions = KeyboardOptions(
+                            autoCorrect = true,
+                            capitalization = KeyboardCapitalization.Sentences,
+                            keyboardType = KeyboardType.Text
+                        ),
                         singleLine = true,
-                        modifier = Modifier.fillMaxWidth().padding(8.dp)
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
                     )
                 }
             }
         )
     }
+
+    if (showEditDialog && editTask != null) {
+        AlertDialog(
+            onDismissRequest = { showEditDialog = false },
+            confirmButton = {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween)
+                {
+                    Button(onClick = { showEditDialog = false }) {
+                        Text("Cancel")
+                    }
+
+                    Button(onClick = {
+                        editTask?.let {
+                            val currentDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                                .format(Date())
+
+                            val id = editTask!!.id
+
+                            tasks = tasks.map { t ->
+                                if (t.id == id)
+                                    t.copy(
+                                        name = taskName,
+                                        description = taskDescription,
+                                        date = currentDate
+                                    )
+                                else t
+                            }
+                        }
+
+                        showEditDialog = false
+                    }) {
+                        Text("Save")
+                    }
+                }
+            },
+            title = { Text("Edit Task") },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = taskName,
+                        onValueChange = { taskName = it },
+                        label = { Text("Task Name") },
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(
+                            autoCorrect = true,
+                            capitalization = KeyboardCapitalization.Sentences,
+                            keyboardType = KeyboardType.Text
+                        )
+                    )
+                    OutlinedTextField(
+                        value = taskDescription,
+                        onValueChange = { taskDescription = it },
+                        label = { Text("Description") },
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(
+                            autoCorrect = true,
+                            capitalization = KeyboardCapitalization.Sentences,
+                            keyboardType = KeyboardType.Text
+                        )
+                    )
+                }
+            },
+
+        )
+    }
 }
+
+@Composable
+fun TaskList(
+    item: Tasks,
+    onRowClick: ()-> Unit,
+    onDeleteClick: ()-> Unit,
+    ){
+    Row (  //outer Row for the entire item
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable{onRowClick()}
+            .shadow(
+                elevation = 8.dp,                 // shadow height
+                shape = RoundedCornerShape(20),   // match your border shape
+                clip = false
+            )
+            .background(
+                color = Color(0xFFFFF1F4),          // your desired background color
+                shape = RoundedCornerShape(20)
+            )
+            .border(
+                border = BorderStroke(2.dp, Color(0xFFE91E63)),
+                shape = RoundedCornerShape(20)
+            )
+    ){
+        Column(modifier = Modifier.fillMaxWidth())
+        {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween)
+            {
+                Text(text = item.name,fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.padding(8.dp, 8.dp, 8.dp, 2.dp))
+
+                Text(text = item.date,color = Color.Black, modifier = Modifier.padding(10.dp))
+            }
+
+            Text(text = item.description,color = Color.Black, modifier = Modifier.padding(10.dp, 0.dp, 8.dp, 8.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "To-Do List App Preview")
+@Composable
+fun ToDoListAppPreview() {
+    // We call the main app composable for the preview
+    ToDoListApp()
+}
+
+
