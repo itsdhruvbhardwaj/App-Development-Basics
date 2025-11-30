4369d35c8ea5543559f2e0a01bf029a7d94e7bcb
realdhruvsharmaa <itsdhruvbhardwaj@gmail.com>
2025-11-30 18:08:58 +0530

    Started with Recipe App, to understand APIs

---
+++ My Recipe App/app/src/main/java/com/hackathon/myrecipeapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.hackathon.myrecipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hackathon.myrecipeapp.ui.theme.MyRecipeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyRecipeAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize())
+                { innerPadding ->
+                    Surface(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    )
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyRecipeAppTheme {
+        Greeting("Android")
+    }
+}
