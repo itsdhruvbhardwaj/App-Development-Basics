c0bbe23e80786f2ce2aa82bbf7e29fc7dc26e709
Dhruv Sharma <itsdhruvbhardwaj@gmail.com>
2026-03-31 19:48:11 +0530

    added apk file

--- MyApplication/app/src/main/java/com/example/myapplication/model/Recipe.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.model
-
-data class Recipe(
-    val id: String,
-    val title: String,
-    val imageUrl: String,
-    val ingredients: List<String>,
-    val instructions: List<String>
-)
